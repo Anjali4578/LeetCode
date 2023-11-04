359c23f5fc0cc359c2404d96e4d5f56ce5897e16
Anjali <86060577+Anjali4578@users.noreply.github.com>
2023-11-04 17:30:16 +0530

    Time: 6 ms (96.37%) | Memory: 14.2 MB (5.05%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        int cnt[128] = {};
+    /*    int cnt[128] = {};
         for (char c: s) {
             cnt[c] += 1;
         }
@@ -10,6 +10,29 @@ public:
             return cnt[a] > cnt[b] || (cnt[a] == cnt[b] && a < b);
         });
 
-        return s;
+        return s;  */
+
+        // bucket sort
+
+        int n = s.size();
+        unordered_map<char, int> cnt;
+
+        for (char c: s) {
+            cnt[c]++;
+        }
+
+        vector<vector<char>> bucket(n + 1);
+        for (auto [c, f]: cnt) {
+            bucket[f].push_back(c);  // char with same frequency grouped together
+        }
+
+        string ans = "";
+        for (int freq = n; freq >= 1; freq--) {
+            for (char c: bucket[freq]) {
+                ans.append(freq, c);
+            }
+        }
+
+        return ans;
     }
 };
