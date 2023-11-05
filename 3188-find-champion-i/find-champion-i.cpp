6ea26be1273dcc6cac937d0ab6ceef852d86883f
Anjali <86060577+Anjali4578@users.noreply.github.com>
2023-11-05 10:44:35 +0530

    Time: 71 ms (14.29%) | Memory: 36.6 MB (42.86%) - LeetSync

---
+++ 3188-find-champion-i/find-champion-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findChampion(vector<vector<int>>& grid) {
+        int n = grid.size();
+        for (int i = 0; i < n; i++) {
+            int tmp = 0;
+            for (int j = 0; j < n; j++) {
+                tmp += grid[i][j]; 
+            }
+
+            if (tmp == n - 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
