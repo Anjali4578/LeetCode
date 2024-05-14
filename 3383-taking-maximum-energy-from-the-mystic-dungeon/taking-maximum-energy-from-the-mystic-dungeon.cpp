b7d0d15532296de0d4c2bbe2b8fa72fa9b915f02
Anjali <86060577+Anjali4578@users.noreply.github.com>
2024-05-14 15:31:36 +0530

    Time: 145 ms (56.20%) | Memory: 149.7 MB (41.59%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
-        int n = energy.size();
-        vector<vector<int>> a(n + 1);
+        // int n = energy.size();
+        // vector<vector<int>> a(n + 1);
 
-        int index = 0;
-        for (int i = 0; i < n; i++) {  // O(n)
-            a[index].push_back(energy[i]);
-            index++;
-            if (index == k) index = 0;
-        }
+        // int index = 0;
+        // for (int i = 0; i < n; i++) {  // O(n)
+        //     a[index].push_back(energy[i]);
+        //     index++;
+        //     if (index == k) index = 0;
+        // }
 
-        int ans = INT_MIN;
-        for (auto it: a) { // O(k)
-            if (it.size() > 0) {
-                int suffix_sum = 0;
-                for (int i = it.size() - 1; i >= 0; i--) {  // O(n / k)
-                    suffix_sum += it[i];
-                    ans = max(ans, suffix_sum);
-                }
-            }
-        }
+        // int ans = INT_MIN;
+        // for (auto it: a) { // O(k)
+        //     if (it.size() > 0) {
+        //         int suffix_sum = 0;
+        //         for (int i = it.size() - 1; i >= 0; i--) {  // O(n / k)
+        //             suffix_sum += it[i];
+        //             ans = max(ans, suffix_sum);
+        //         }
+        //     }
+        // }
 
-        return ans;
+        // return ans;
 
         // T.C. = O(n)
         // S.C. = O(k * n / k) ~ O(n)
+
+        int n = energy.size();
+        vector<int> dp(n);
+
+        int ans = INT_MIN;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = energy[i] + (i + k < n ? dp[i + k] : 0);
+            ans = max(ans, dp[i]);
+        }
+
+        return ans;
     }
 };
