3313164b02d721df30afa1bb33da08f034b505bb
Anjali <86060577+Anjali4578@users.noreply.github.com>
2025-04-12 16:20:03 +0530

    Time: 2 ms (80.27%) | Memory: 48.3 MB (95.86%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -3,18 +3,31 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> ans;
         int n = nums.size();
-        unordered_map<int, int> mp;
+        // unordered_map<int, int> mp;
+        // for (int i = 0; i < n; i++) {
+        //     mp[nums[i]]++;
+        // }
+
+        // for (auto it: mp) {
+        //     if (it.second == 2) {
+        //         ans.push_back(it.first);
+        //     }
+        // }
+        // return ans;
+
+        // T.C. = O(N)  S.C. = O(N) -> map
+
         for (int i = 0; i < n; i++) {
-            mp[nums[i]]++;
+            while (nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
         }
 
-        for (auto it: mp) {
-            if (it.second == 2) {
-                ans.push_back(it.first);
-            }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) ans.push_back(nums[i]);
         }
         return ans;
 
-        // T.C. = O(N)  S.C. = O(N) -> map
+        // T.C. = O(N)   S.C. = O(1)
     }
 };
