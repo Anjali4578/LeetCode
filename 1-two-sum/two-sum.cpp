db8028b57c8efe1ada0a42a80d42b7f57d8a1ec3
Anjali <86060577+Anjali4578@users.noreply.github.com>
2025-04-12 14:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (36.12%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,14 +3,29 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
         int n = nums.size();
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
+        // return ans;
+        // T.C. = O(N ^ 2)  S.C.-> for ans
+
+        unordered_map<int, int> mp;
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+            int rem = target - nums[i];
+            if (mp.find(rem) != mp.end()) {
+                ans.push_back(i);
+                ans.push_back(mp[rem]);
+                return ans;
             }
+            mp[nums[i]] = i;
         }
-        return ans;
+        return {-1,-1};
+
+        // T.C. = O(N)   S.C. = O(N) -> map
     }
 };
