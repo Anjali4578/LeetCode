4720fdde87d4116726b02465f6543c42c66e1234
Anjali <86060577+Anjali4578@users.noreply.github.com>
2025-04-13 12:37:29 +0530

    Time: 1 ms (51.94%) | Memory: 11.5 MB (93.28%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSub(string s) {
+        int ans = 0;
+        int cnt = 0, mod = 1e9 + 7;
+        for (char c: s) {
+            if (c == '1') cnt++;
+            else {
+                cnt = 0;
+            }
+            ans =  (ans + cnt) % mod;
+        }
+        return ans;
+    }
+};
