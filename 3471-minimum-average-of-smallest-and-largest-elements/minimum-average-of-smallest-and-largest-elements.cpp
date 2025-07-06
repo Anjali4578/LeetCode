1a4d99b0526e2f82827b91dfce93fae3626cf4ac
Anjali <86060577+Anjali4578@users.noreply.github.com>
2025-07-07 00:05:28 +0530

    Time: 1 ms (25.91%) | Memory: 25.7 MB (37.60%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -4,12 +4,16 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<double> averages;
-
+        double ans = INT_MAX;
         for (int i = 0; i < n / 2; i++) {
             double avg = (nums[i] + nums[n - i - 1]) / 2.0;
             averages.push_back(avg);
+            ans = min(ans, avg);
         }
 
         return *min_element(averages.begin(), averages.end());
+
+        // T.C. = O(n log n)
+        // S.C. = O(m) -> for averages array
     }
 };
